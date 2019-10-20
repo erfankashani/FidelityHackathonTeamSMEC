01b2e138b47a23a3aeb843146b8af4871bfa2462
Saamer Mansoor <i@saamer.me>
2019-10-20 14:06:39 -0400

    Change to the animation

--- Final/script.js
+++ Final/script.js
@@ -28,13 +28,13 @@ var svgContainer = document.getElementById('svgContainer');
 var animItem = bodymovin.loadAnimation({
   wrapper: svgContainer,
   animType: 'svg',
+  autoplay: false,
   loop: false,
   path: 'https://raw.githubusercontent.com/saamerm/FidelityHackathonTeamSMEC/master/Lottie/data2.json'
 });
 function Reverse() {
   console.log("ADG");
-  x *= -1;
-  animItem.setSpeed(x);
+  animItem.play();
 }
 
 $('.btn').click(function () {
